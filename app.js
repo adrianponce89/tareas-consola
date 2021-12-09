3902e38bd734e8a4153bcf9ce973959e0ead22d1
Adrian Ponce <adrian.ponce.89@gmail.com>
2021-12-09 17:23:19 -0300

    Mostrar menu

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 require("colors");
+const mensajes = require("./helpers/mensajes");
 
 const main = async () => {
-  console.clear();
-  console.log("Hola mundo".green);
+  let opcion = "";
+  do {
+    console.clear();
+    opcion = await mensajes.mostrarMenu();
+    switch (opcion) {
+      case "1":
+        crearTarea();
+        break;
+      case "2":
+        listarTareas();
+        break;
+      case "3":
+        listarTareasCompletadas();
+        break;
+      case "4":
+        listarTareasPendientes();
+        break;
+      case "5":
+        completarTarea();
+        break;
+      case "6":
+        borrarTarea();
+        break;
+      case "0":
+        console.log("Adios".red);
+        break;
+      default:
+        console.log("Opcion incorrecta".red);
+        break;
+    }
+    if (opcion !== "0") {
+      await mensajes.pausa();
+    }
+  } while (opcion !== "0");
 };
 
 main();
